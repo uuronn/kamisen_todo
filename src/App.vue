abc6746f4db0c4778664c4c0ca66b4ae69cb93be
uuronn <uuronprogram@gmail.com>
2021-07-11 01:00:09 +0900

    firstcommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main">
+      <h1>神の宣告</h1>
+      <input v-model="TodoName" placeholder="taskName">
+      <button @click="addTodo">追加</button>
+      <ul class="todos">
+        <li v-for="(todo,i) in todos" :key="i">
+          <input type="checkbox">
+          <span>{{ todo }}</span>
+          <button clss="todos_button" @click="doneTodo">完了</button>
+          <button class="todos_button" @click="deleteTodo(i)">削除</button>
+        </li>
+      </ul>
+    </div>
+    
+    <Option msg="optionPage"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Option from './components/Option.vue';
 
 export default {
-  name: 'App',
+  name: "main",
+  data() {
+    return {
+      todos: [],
+    }
+  },
   components: {
-    HelloWorld
+    Option
+  },
+  methods: {
+    // todosにタスクを追加するメソッド
+    addTodo() {
+      if (this.TodoName) {
+        this.todos.push(this.TodoName)
+        this.TodoName = ""
+      }
+    },
+    // 未実装
+    doneTodo() {
+      
+    },
+    // todosのタスクを削除するメソッド
+    deleteTodo(i) {
+      this.todos.splice(i,1)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    display: flex;
+    justify-content: center;
+  }
+
+  .main {
+    flex-flow: column;
+    text-align: center;
+  }
+  
+  h1 {
+    padding: 30px 0;
+  }
+
+  .todos {
+    text-align: left;
+  }
 </style>
---
+++ src/components/Option.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="option">
+    <h3>{{ mode }}モード</h3>
+    <div class="option_top">
+      <div class="option_top_button" @click="openOption" >
+        <img class="option_top_button-img" src="../assets/option_button-img.png" alt="ハンバーガーメニュー画像">
+      </div>
+      <div class="option_top_content" v-if="optionIsShow">
+        <div class="content_mode">
+          <button @click="switchButton">切り替え</button>
+          <button v-if="buttonMenu" @click="defaultMode">デフォルトモード</button>
+          <button v-if="buttonMenu" @click="dissMode">煽りモード</button>
+          <button v-if="buttonMenu" @click="muscleMode">筋トレモード</button>
+        </div>
+        <div class="content_x">
+          <button>aaaa</button>
+          <button>ddddd</button>
+          <button>fdfdfd</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "option",
+  data() {
+    return {
+      mode: 'デフォルト',
+      buttonMenu: false,
+      optionIsShow: false
+    }
+  },
+  methods: {
+    // モードを切り替えるボタンを表示させるメソッド
+    switchButton() {
+        this.buttonMenu = !this.buttonMenu
+    },
+    // デフォルトモードにするメソッド
+    defaultMode() {
+      this.mode = "デフォルト"
+      this.buttonMenu = false
+    },
+    // 煽りモードにするメソッド
+    dissMode() {
+      this.mode = "煽り"
+      this.buttonMenu = false
+    },
+    // 筋トレモードにするメソッド
+    muscleMode() {
+      this.mode = "筋トレ"
+      this.buttonMenu = false
+    },
+    openOption() {
+      this.optionIsShow = !this.optionIsShow
+    }
+  }
+}
+</script>
+
+<style>
+  .option {
+    display: flex;
+    position: absolute;
+    right: 4%;
+  }
+
+  .option_top {
+    display: flex;
+    flex-flow: column;
+    text-align: right;
+    width: 500px;
+  }
+
+  .option_top_button {
+    
+  }
+
+  .option_top_button-img {
+    vertical-align: top;
+    width: 50px;
+    transition: 1s;
+  }
+
+  .option_top_button-img:hover {
+    border-radius: 20px;
+    transition: 1s;
+  }
+
+  .option_top_content {
+    display: flex;
+    max-width: 120px;
+  }
+
+  button {
+    min-width: 130px;
+  }
+
+</style>
